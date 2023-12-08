815aa52f7589300098ab5ba7a83b17e0b20f6259
Jose Miguel Contro <josemiguel_contro@epam.com>
2023-12-08 12:03:20 -0600

    Task 3 commit

---
+++ product-service/mockProducts.js
@@ -0,0 +1,12 @@
+const mockProducts = [
+    { id: 1, title: 'Monitor IPS 23 inches', price: 155},
+    { id: 2, title: 'Monitor IPS 27 inches', price: 210},
+    { id: 3, title: 'Monitor TN 24 inches', price: 110},
+    { id: 4, title: 'Monitor TN 27 inches', price: 140},
+    { id: 5, title: 'Monitor VA 23 inches', price: 99},
+    { id: 6, title: 'Monitor VA 27 inches', price: 145},
+];
+
+module.exports = mockProducts;
+
+
